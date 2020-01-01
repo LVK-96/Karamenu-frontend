51e433f0d63fd8cf8eb048af559ecda69d82c01a
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-01 19:11:35 +0200

    Dummy data for development

--- src/Services/menu.js
+++ src/Services/menu.js
@@ -1,16 +1,95 @@
 import axios from 'axios';
 
+const dummyMenu = {
+  "date": "2020-01-03",
+  "courses": [
+    {
+      "category": "",
+      "name_fi": "Punajuurisosekeittoa",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "* ,A ,G ,L",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Mantelia",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "A ,G ,L ,M ,Veg",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Kasvisbolognesea",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "* ,A ,G ,L ,M ,Veg ,VS",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Spagettia",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "A ,L ,M ,Veg",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Vegaanista juustoraastetta",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "G ,L ,M ,Veg",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Uunikirjolohta",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "* ,A ,G ,L ,M",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Kermaperunoita",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "A ,G ,L",
+      "price": ""
+    },
+    {
+      "category": "",
+      "name_fi": "Höyrytettyä parsakaalia",
+      "name_en": "",
+      "desc_fi": "",
+      "desc_en": "",
+      "tags": "* ,G ,L ,M ,Veg",
+      "price": ""
+    }
+  ]
+};
+
 const baseUrl = `${process.env.REACT_APP_BACKEND_URI}/restaurants`;
 
-const getOne = async (restaurant, date) => {
+const getByIdAndDate= async (restaurantId, date) => {
   try {
-    const url = `${baseUrl}/${restaurant.id}/${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    const url = `${baseUrl}/${restaurantId}/${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     const response = await axios.get(url);
     return response.data;
   } catch (exception) {
     console.log(exception);
-    return null;
+    if (process.env.REACT_APP_DEV) return dummyMenu;
+    throw exception;
   }
 }
 
-export default { getOne };
+export default { getByIdAndDate };
--- src/Services/restaurant.js
+++ src/Services/restaurant.js
@@ -1,5 +1,35 @@
 import axios from 'axios';
 
+const dummyRestaurants = [
+  {
+    "id": 1,
+    "name": "Midpoint",
+    "company": "Sodexo",
+    "address": "Karakaari 7",
+    "opens": "10:30:00",
+    "closes": "13:45:00",
+    "url": "https://www.sodexo.fi/midpoint"
+  },
+  {
+    "id": 2,
+    "name": "Nokia One Espoo",
+    "company": "Sodexo",
+    "address": "Karaportti 3",
+    "opens": "10:30:00",
+    "closes": "13:45:00",
+    "url": "https://www.sodexo.fi/nokiaone"
+  },
+  {
+    "id": 3,
+    "name": "Dreams Cafe",
+    "company": "Fazer",
+    "address": "Karaportti 4",
+    "opens": "11:00:00",
+    "closes": "13:30:00",
+    "url": "https://www.fazerfoodco.fi/ravintolat/Ravintolat-kaupungeittain/espoo/dreams-cafe/"
+  }
+];
+
 const baseUrl = `${process.env.REACT_APP_BACKEND_URI}/restaurants`;
 
 const getAll = async () => {
@@ -8,18 +38,19 @@ const getAll = async () => {
     return response.data;
   } catch (exception) {
     console.log(exception);
-    return null;
+    if (process.env.REACT_APP_DEV) return dummyRestaurants;
+    throw exception;
   }
 }
 
-const getOne = async (restaurant) => {
+const getById= async (restaurantId) => {
   try {
-    const response = await axios.get(`${baseUrl}/${restaurant.id}`);
+    const response = await axios.get(`${baseUrl}/${restaurantId}`);
     return response.data;
   } catch (exception) {
     console.log(exception);
-    return null;
+    throw exception;
   }
 }
 
-export default { getAll, getOne };
+export default { getAll, getById };
