239043c88e339400adfd3e5cb0dde68c533ca58b
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2020-01-06 18:04:14 +0200

    Add loading icons

--- src/Components/MainView/MainView.js
+++ src/Components/MainView/MainView.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
 import './MainView.css';
 import restaurantService from '../../Services/restaurant';
 import MenuCard from '../MenuCard';
 import DateSelector from '../DateSelector';
+
+const MenuCards = ({ restaurants, date, dummy }) => {
+  if (restaurants.length > 0) {
+    return (
+      <div className='menuCards'>
+        {restaurants.map(r => <MenuCard key={r.id} date={date} restaurant={r} dummy={dummy} />)}
+      </div>
+    );
+  } else {
+      return (
+        <Spinner animation="grow" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+  }
+}
 
 const MainView = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -28,9 +45,7 @@ const MainView = () => {
       <div className='dateSelector'>
         <DateSelector date={date} setDate={setDate} dummy={dummy} setDummy={setDummy} />
       </div>
-      <div className='menuCards'>
-        {restaurants.map(r => <MenuCard key={r.id} date={date} restaurant={r} dummy={dummy} />)}
-      </div>
+      <MenuCards restaurants={restaurants} date={date} dummy={dummy} />
     </div>
   );
 };
--- src/Components/MenuCard/MenuCard.js
+++ src/Components/MenuCard/MenuCard.js
@@ -1,9 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { ListGroup, Card } from 'react-bootstrap';
+import { ListGroup, Card, Spinner } from 'react-bootstrap';
 import uuidv4 from 'uuid';
 import menuService from '../../Services/menu';
 import Course from '../Course';
 import './MenuCard.css';
+
+const Header = ({ restaurant }) => {
+  if (restaurant) {
+    return (
+      <Card.Header as='h5'>
+        <div>
+          <Card.Link href={restaurant.url}>{restaurant.name}</Card.Link>
+        </div>
+        <div>
+          {`${restaurant.opens} - ${restaurant.closes}`}
+        </div>
+      </Card.Header>
+    );
+  } else {
+    return (
+      <Card.Header as='h5'>
+        <div>
+          Loading...
+        </div>
+      </Card.Header>
+    );
+  }
+};
+
+const Body = ({ menu }) => {
+  if (menu) {
+    return (
+      <ListGroup variant='flush'>
+        {menu.courses.map(c => <Course key={uuidv4()} course={c} />)}
+      </ListGroup>
+    );
+  } else {
+    return (
+      <Card.Body>
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      </Card.Body>
+    );
+  }
+};
 
 const MenuCard = ({date, restaurant, dummy}) => {
   const [menu, setMenu] = useState(null);
@@ -20,28 +61,16 @@ const MenuCard = ({date, restaurant, dummy}) => {
       }
     }
 
+    setMenu(null);
     getMenu();
   }, [restaurant, date, dummy]);
 
-  if (menu) {
-    return (
-      <Card className='menuCardContainer'>
-        <Card.Header as='h5'>
-          <div>
-            <Card.Link href={restaurant.url}>{restaurant.name}</Card.Link>
-          </div>
-          <div>
-            {`${restaurant.opens} - ${restaurant.closes}`}
-          </div>
-        </Card.Header>
-        <ListGroup variant='flush'>
-          {menu.courses.map(c => <Course key={uuidv4()} course={c} />)}
-        </ListGroup>
-      </Card>
-    )
-  } else {
-    return <></>
-  }
+  return (
+    <Card className='menuCardContainer'>
+      <Header restaurant={restaurant} />
+      <Body menu={menu} />
+    </Card>
+  );
 }
 
 export default MenuCard;
